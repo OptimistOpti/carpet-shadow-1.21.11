7ea3bb31cee4cdfb3e96eafd2320aee3a325209f
OptimistOpti <lordsergei337@gmail.com>
2026-07-26 11:31:38 +0300

    Add files via upload

---
+++ src/main/java/com/carpet_shadow/mixins/inv_updates/loaders/BlockEntityMixin.java
@@ -0,0 +1,34 @@
+package com.carpet_shadow.mixins.inv_updates.loaders;
+
+
+import com.carpet_shadow.interfaces.InventoryItem;
+import com.carpet_shadow.interfaces.ShadowItem;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(BlockEntity.class)
+public abstract class BlockEntityMixin {
+
+    @Inject(method = "createFromNbt",at=@At("RETURN"))
+    private static void interceptBlockEntityLoad(BlockPos pos, BlockState state, NbtCompound nbt, CallbackInfoReturnable<BlockEntity> cir){
+        if(cir.getReturnValue() instanceof Inventory inv){
+            try {
+                for (int index = 0; index < inv.size(); index++) {
+                    ItemStack stack = inv.getStack(index);
+                    if (((ShadowItem) (Object) stack).carpet_shadow$getShadowId() != null) {
+                        ((InventoryItem) (Object) stack).carpet_shadow$addSlot(inv, index);
+                    }
+                }
+            }catch(Exception ignored){}
+        }
+    }
+
+}
