9e0fcf13708f787c57d989f7561def1c4a24500f
OptimistOpti <lordsergei337@gmail.com>
2026-07-26 11:15:24 +0300

    Add files via upload

---
+++ src/main/java/com/carpet_shadow/CarpetShadow.java
@@ -0,0 +1,75 @@
+package com.carpet_shadow;
+
+import carpet.CarpetExtension;
+import carpet.CarpetServer;
+import com.carpet_shadow.utility.RandomString;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.llamalad7.mixinextras.MixinExtrasBootstrap;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.MinecraftServer;
+import org.apache.commons.io.IOUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.Map;
+
+// NOTE [PORT 1.21.11] see PORTING_NOTES.md #7.
+//  CarpetExtension's API has evolved a bit across carpet 1.4.11x -> 1.4.194 (e.g. the deprecated
+//  CarpetExtension::customSettingsManager was removed in favor of extensionSettingsManager).
+//  Double check this class + CarpetExtension's interface for removed/renamed default methods.
+public class CarpetShadow implements CarpetExtension, ModInitializer {
+    public static final Cache<String, ItemStack> shadowMap = CacheBuilder.newBuilder().weakValues().build();
+    public static final Logger LOGGER = LogManager.getLogger("carpet-shadow");
+    public static RandomString shadow_id_generator = new RandomString(CarpetShadowSettings.shadowItemIdSize);
+
+
+
+    @Override
+    public void onGameStarted() {
+        CarpetShadow.LOGGER.info("Carpet Shadow Loaded!");
+        CarpetServer.settingsManager.parseSettingsClass(CarpetShadowSettings.class);
+        shadow_id_generator = new RandomString(CarpetShadowSettings.shadowItemIdSize);
+    }
+
+    @Override
+    public void onInitialize() {
+        CarpetServer.manageExtension(new CarpetShadow());
+        CarpetShadow.LOGGER.info("Carpet Shadow Loading!");
+    }
+
+    @Override
+    public void onServerLoaded(MinecraftServer server) {
+        shadowMap.invalidateAll();
+    }
+
+    @Override
+    public void onTick(MinecraftServer server) {
+        if (server.getTicks() % 1000 == 0)
+            CarpetShadow.shadowMap.cleanUp();
+    }
+
+    @Override
+    public Map<String, String> canHasTranslations(String lang) {
+        InputStream langFile = getClass().getClassLoader().getResourceAsStream("assets/carpet-shadow/lang/%s.json".formatted(lang));
+        if (langFile == null) {
+            return Collections.emptyMap();
+        }
+        String jsonData;
+        try {
+            jsonData = IOUtils.toString(langFile, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            return Collections.emptyMap();
+        }
+        Gson gson = new GsonBuilder().setLenient().create();
+        return gson.fromJson(jsonData, new TypeToken<Map<String, String>>() {}.getType());
+    }
+}
