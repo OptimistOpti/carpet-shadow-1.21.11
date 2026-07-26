e2fb6f2c00f68c9451cc0951cafe229fbefaa2cd
OptimistOpti <lordsergei337@gmail.com>
2026-07-26 18:40:45 +0300

    Update ServerPlayerInteractionManagerMixin.java

--- src/main/java/com/carpet_shadow/mixins/hand_update_fix/ServerPlayerInteractionManagerMixin.java
+++ src/main/java/com/carpet_shadow/mixins/hand_update_fix/ServerPlayerInteractionManagerMixin.java
@@ -28,7 +28,7 @@ public abstract class ServerPlayerInteractionManagerMixin {
             ActionResult result = cir.getReturnValue();
             if (result==ActionResult.SUCCESS || result == ActionResult.CONSUME) {
                 int index = (hand == Hand.OFF_HAND) ? PlayerInventory.OFF_HAND_SLOT : player.getInventory().getSelectedSlot();
-                player.currentScreenHandler.getSlotIndex(player.getInventory(), index).ifPresent(i -> player.currentScreenHandler.setPreviousTrackedSlot(i, new ItemStack(Blocks.AIR)));
+                player.currentScreenHandler.getSlotIndex(player.getInventory(), index).ifPresent(i -> player.currentScreenHandler.updateTrackedSlot(i, new ItemStack(Blocks.AIR), () -> new ItemStack(Blocks.AIR)));
             }
         }
     }
