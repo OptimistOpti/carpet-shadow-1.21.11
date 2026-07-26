54197f76507a36980d3e58ba30cc059987dd0b16
OptimistOpti <lordsergei337@gmail.com>
2026-07-26 11:16:27 +0300

    Add files via upload

---
+++ src/main/java/com/carpet_shadow/interfaces/ItemEntitySlot.java
@@ -0,0 +1,11 @@
+package com.carpet_shadow.interfaces;
+
+import net.minecraft.entity.ItemEntity;
+
+public interface ItemEntitySlot {
+
+    ItemEntity carpet_shadow$getEntity();
+
+    void carpet_shadow$setEntity(ItemEntity entity);
+
+}
---
+++ src/main/java/com/carpet_shadow/interfaces/ShifingItem.java
@@ -0,0 +1,6 @@
+package com.carpet_shadow.interfaces;
+
+public interface ShifingItem {
+    boolean carpet_shadow$isShiftMoving();
+    void carpet_shadow$setShiftMoving(boolean val);
+}
